4536440d8bc497be20084c2c56503f27a87385e9
dvrtnkv <dv@collarslab.com>
2022-11-30 18:29:02 +0500

    Worker added

--- pages/houses.tsx
+++ pages/houses.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../components/Layout";
+import AddHouses from "../components/AddHouses";
 
 export default function Home() {
 	return (
@@ -10,6 +10,7 @@ export default function Home() {
 				<meta name="description" content="Запросить жильё в аренду по параметрам" />
 				<link rel="canonical" href="https://collarslab.com/houses" />
 			</Head>
+			<AddHouses />
 		</Layout>
 	);
 }
