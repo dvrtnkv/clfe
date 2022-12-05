e337feaa53b4a3b149cac6f1d0b2232f58e3c86b
dvrtnkv <robert@collarslab.com>
2022-12-05 22:23:29 +0500

    Unexpected token . Expected jsx identifier: FIXED

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,22 +5,24 @@ import Nav from "./Nav";
 
 export default function Layout({children}: {children: React.ReactNode}) {
 	return (
-		<main className="h-screen grid mobile-template lg:main-template font-sans antialiased">
-			<Head>
-				<meta charSet="utf-8" />
-				<title>Collars Lab.</title>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<meta name="theme-color" content="#ffffff" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-				<link rel="manifest" href="/manifest.json" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Header />
-			<Nav />
-			<section className="page-content container px-8 pt-4">{children}</section>
-			<Footer />
-		</main>
+		<>
+			<main className="h-screen grid mobile-template lg:main-template font-sans antialiased">
+				<Head>
+					<meta charSet="utf-8" />
+					<title>Collars Lab.</title>
+					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+					<meta name="theme-color" content="#ffffff" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<Header />
+				<Nav />
+				<section className="page-content container px-8 pt-4">{children}</section>
+				<Footer />
+			</main>
+		</>
 	);
 }
